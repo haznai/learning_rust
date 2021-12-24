2a473c74c21a20749ba2c42f7fbae580f5c6bfe6
haznai <aitakprojects@gmail.com>
2021-12-24 18:17:51 +0100

    aoc 2019 day 2

--- aoc_2019/src/main.rs
+++ aoc_2019/src/main.rs
@@ -1,7 +1,14 @@
 mod day01;
-use crate::day01::Day01Solution;
-use day01::Solution;
+use day01::{Day01Solution, Solution};
+
+mod day02;
+use day02::Day02solution;
+
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
 
 fn main() {
     Day01Solution::print_solution("input/day01.txt");
+    Day02solution::print_solution("input/day02.txt");
 }
